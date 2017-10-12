97101a0566765a3c089bdf1b2975650928940948
Corey Frang <gnarf37@gmail.com>
2017-10-12 17:43:19 -0400

    use util.inspect

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fallback = require('express-history-api-fallback');
 var app = express();
 var path = require('path');
-
+var util = require('util');
 
 // set the port
 var port = process.env.PORT || 8080;
@@ -11,7 +11,7 @@ var port = process.env.PORT || 8080;
 // @todo: This could be improved to serve development vs production builds.
 var root = path.join(__dirname, '../client/dist');
 app.get('/env', (req, res) => {
-  res.send(JSON.stringify({ env: process.env, req }, true, '  '));
+  res.send(util.inspect({ env: process.env, req }));
 })
 app.use(express.static(root));
 app.use(fallback('index.html', {root: root}));
